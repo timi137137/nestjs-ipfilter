e815ae7127c877c0106f1c3e185ba368201c78ad
悠静萝莉 <i@mikuhl.cn>
2024-01-10 01:18:58 +0800

    chore: fix exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,7 @@
 export * from './interfaces';
+export * from './ip-filter.constants';
 export * from './ip-filter.guard';
+export * from './ip-filter.module-definition';
+export * from './ip-filter.service';
 export * from './ip-filter-module';
+export * from './utils';
