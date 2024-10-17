c6e8b6af80bcaf41c313e7d6d7904ab89be4ef38
悠静萝莉 <i@mikuhl.cn>
2024-10-18 00:44:02 +0800

    chore: update ver

--- lib/ip-filter.guard.ts
+++ lib/ip-filter.guard.ts
@@ -26,7 +26,10 @@ export class IpFilterGuard implements CanActivate {
     context: ExecutionContext,
   ): Promise<boolean> | Observable<boolean> | boolean {
     // get guard
-    const guard = this.reflector.getAllAndOverride<boolean>('ipFilter', [context.getHandler(), context.getClass()]);
+    const guard = this.reflector.getAllAndOverride<boolean>('ipFilter', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
     // if not global and guard is not defined, then skip ip filter
     if (!this.ipFilterService.isGlobal && guard === undefined) {
